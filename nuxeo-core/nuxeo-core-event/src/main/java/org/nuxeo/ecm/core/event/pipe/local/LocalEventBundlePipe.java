cfe5c37d9e2f1de2214daee802215a4f02b0f62c
Thierry Delprat <tdelprat@nuxeo.com>
2016-03-15 23:19:19 -0400

    NXP-19222 share initial structure

--- nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/event/pipe/local/LocalEventBundlePipe.java
+++ nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/event/pipe/local/LocalEventBundlePipe.java
@@ -1,41 +1,37 @@
 /*
- * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
  * Contributors:
  *     tiry
  */
 package org.nuxeo.ecm.core.event.pipe.local;
 
-import org.nuxeo.common.annotation.Experimental;
-import org.nuxeo.ecm.core.event.EventBundle;
-import org.nuxeo.ecm.core.event.pipe.AbstractEventBundlePipe;
-import org.nuxeo.ecm.core.event.pipe.EventBundlePipe;
-
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.nuxeo.ecm.core.event.EventBundle;
+import org.nuxeo.ecm.core.event.pipe.AbstractEventBundlePipe;
+import org.nuxeo.ecm.core.event.pipe.EventBundlePipe;
+import org.nuxeo.ecm.core.event.pipe.PipeConsumer;
+
 /**
  * Local In memory implementation: directly relays to WorkManager
- *
- * @since 8.4
+ * @since TODO
  */
-@Experimental
 public class LocalEventBundlePipe extends AbstractEventBundlePipe<EventBundle> implements EventBundlePipe {
 
-    protected LocalEventBundlePipeConsumer consumer;
+    protected PipeConsumer<EventBundle> consumer;
 
     @Override
     public void initPipe(String name, Map<String, String> params) {
@@ -46,18 +42,16 @@ public class LocalEventBundlePipe extends AbstractEventBundlePipe<EventBundle> i
 
     @Override
     protected void send(EventBundle message) {
-        List<EventBundle> messages = Collections.singletonList(message);
+        List<EventBundle> messages = new ArrayList<EventBundle>();
+        messages.add(message);
         consumer.receiveMessage(messages);
     }
 
     @Override
     protected EventBundle marshall(EventBundle events) {
+        //ReconnectedEventBundle bundle = new ReconnectedEventBundleImpl(events);
+        //bundle.disconnect();
         return events;
-    }
-
-    @Override
-    public boolean waitForCompletion(long timeoutMillis) throws InterruptedException {
-        return consumer.waitForCompletion(timeoutMillis);
     }
 
 }
--- nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/event/pipe/local/LocalEventBundlePipeConsumer.java
+++ nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/event/pipe/local/LocalEventBundlePipeConsumer.java
@@ -1,17 +1,15 @@
 /*
- * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
  * Contributors:
  *     tiry
@@ -20,25 +18,20 @@ package org.nuxeo.ecm.core.event.pipe.local;
 
 import java.util.List;
 
-import javax.resource.spi.work.WorkManager;
-
-import org.nuxeo.common.annotation.Experimental;
 import org.nuxeo.ecm.core.event.EventBundle;
 import org.nuxeo.ecm.core.event.pipe.AbstractListenerPipeConsumer;
 
 /**
- * In memory implementation that does not handle any marshaling and directly feeds the {@link WorkManager}
  *
- * @since 8.4
+ * @since TODO
  */
-@Experimental
 public class LocalEventBundlePipeConsumer extends AbstractListenerPipeConsumer<EventBundle> {
 
     @Override
     protected List<EventBundle> unmarshallEventBundle(List<EventBundle> messages) {
-        // xxx reconnect them all using the same session ?
-        // we may not want to reconnect at this stage and let the workers do it
-        return messages;
+       // xxx reconnect them all using the same session ?
+       // we may not want to reconnect at this stage and let the workers do it
+       return messages;
     }
 
 }
