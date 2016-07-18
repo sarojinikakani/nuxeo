0601337eccb6f0eb4aac15117b683641e723161e
Thierry Delprat <tdelprat@nuxeo.com>
2016-07-18 15:46:05 -0400

    NXP-19222 WIP : run some tests against the Pipe based implementation

--- nuxeo-core/nuxeo-core-event/src/test/java/org/nuxeo/ecm/core/event/pipe/TestEventListenerViaQueue.java
+++ nuxeo-core/nuxeo-core-event/src/test/java/org/nuxeo/ecm/core/event/pipe/TestEventListenerViaQueue.java
@@ -1,22 +1,22 @@
 /*
- * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
  * Contributors:
  *     tiry
  */
 package org.nuxeo.ecm.core.event.pipe;
+
+import java.net.URL;
 
 import org.junit.Before;
 import org.nuxeo.ecm.core.event.impl.PostCommitEventExecutor;
@@ -24,8 +24,6 @@ import org.nuxeo.ecm.core.event.test.DummyPostCommitEventListener;
 import org.nuxeo.ecm.core.event.test.EventListenerTest;
 import org.nuxeo.ecm.core.event.test.TestEventServiceComponent;
 import org.nuxeo.runtime.api.Framework;
-
-import java.net.URL;
 
 /**
  * Run the existing EventListeners tests using the Queue implementation.
@@ -49,7 +47,7 @@ public class TestEventListenerViaQueue extends TestEventServiceComponent {
         Framework.getProperties().setProperty(PostCommitEventExecutor.TIMEOUT_MS_PROP, "300"); // 0.3s
         deployBundle("org.nuxeo.runtime.jtajca");
         deployBundle("org.nuxeo.ecm.core.event");
-        URL url = EventListenerTest.class.getClassLoader().getResource("test-LocalQueues.xml");
+        URL url = EventListenerTest.class.getClassLoader().getResource("test-LocalPipes.xml");
         deployTestContrib("org.nuxeo.ecm.core.event.test", url);
 
         fireFrameworkStarted();
